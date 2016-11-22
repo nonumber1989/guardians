1bc394a11a326adacfb3a81f26caffbbc05077b5
steven xu <nonumber1989@gmail.com>
2016-11-22 21:07:21 +0800

    i start up my little dream

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # guardians
-Guardians with rocket raccoon
+Guardians is a little platform for notification 
+
+## Why
+
+  + some application lack notification 
+  + no need for complicated notification environment setup
+  + support push and pull
+
+## How 
+
+  + websockt 
+  + RESTful
+  + Push & Pull combination
+
+ ## Contact
+   [steven.xu](nonumber1989@gmail.com) 
--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-index.js
+var connect = require('connect');
+var http = require('http');
+
+var guardians = connect();
+
+// parse urlencoded request bodies into req.body
+var bodyParser = require('body-parser');
+guardians.use(bodyParser.urlencoded({extended: false}));
+
+// respond to all requests
+guardians.use(function(req, res){
+  res.end('guardians of galaxy!\n');
+});
+
+//create node.js http server and listen on port
+http.createServer(guardians).listen(3000);
