33db2bb6d949b6d1d280d3d4cb5d05930aadd2f0
Steven Xu <nonumber1989@gmail.com>
2016-11-27 21:35:21 +0800

    use namespace & rooms

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
 var connect = require('connect');
 var http = require('http');
+var socketIo = require('socket.io')
+var fs = require('fs');
 
 var guardians = connect();
 
 // parse urlencoded request bodies into req.body
 var bodyParser = require('body-parser');
-guardians.use(bodyParser.urlencoded({extended: false}));
+guardians.use(bodyParser.urlencoded({ extended: false }));
 
 // respond to all requests
-guardians.use(function(req, res){
-  res.end('guardians of galaxy!\n');
+guardians.use(function(req, res) {
+    fs.readFile(__dirname + '/index.html',
+        function(err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
 });
 
 //create http server 
-var server  = http.createServer(guardians);
-var io = require('socket.io')(server);
-io.on('connection', function(client){
-  client.on('event', function(data){});
-  client.on('disconnect', function(){});
+var server = http.createServer(guardians);
+var io = socketIo(server);
+
+
+var chat = io.of('/chat').on('connection', function(socket) {
+    socket.emit('chatMessage', {'fromChat':"here is a message form chat !"});
+    socket.on('chatEvent', function(data) {
+        console.log("chat event ----"+data);
+    });
 });
+
+var news = io.of('/news').on('connection', function(socket) {
+    socket.emit('newsMessage', { 'fronNews': 'here is a message from news !' });
+    socket.on('newsEvent', function(data) {
+        console.log("news event ----"+data);
+    });
+});
+
+
 server.listen(3000);
