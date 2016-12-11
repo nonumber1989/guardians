0cd8d1e1166ea482d6c10ab314634dc42ca004ed
steven xu <nonumber1989@gmail.com>
2016-12-11 11:52:53 +0800

    socket and redis work first

--- guardians.js
+++ guardians.js
@@ -1,7 +1,7 @@
 var socketIo = require('socket.io')
 var socketioJwt = require("socketio-jwt");
 var configuration = require('./configurations/configuration');
-
+var subClient = require('./configurations/redisClient').subClient;
 var io = socketIo(configuration.socketIoPort);
 
 io.of('/chat').on('connection', socketioJwt.authorize({
@@ -16,19 +16,26 @@ io.of('/chat').on('connection', socketioJwt.authorize({
         });
 
         socket.decoded_token.rooms.forEach(function(room) {
-            console.log(room+"---------------nonumber1989-------------------");
-             socket.join(room);
+            console.log(room + "---------------nonumber1989-------------------");
+            socket.join(room);
         });
     });
 
 // now, it's easy to send a message to just the clients in a given room
 room = "abc123";
-setInterval(function() {
-    io.nsps['/chat'].in(room).emit('message', 'what is going on, party people?');
+// setInterval(function() {
+//     io.nsps['/chat'].in(room).emit('message', 'what is going on, party people?');
 
-    io.nsps['/chat'].in('room1').emit('message', 'I was in room one');
-    io.nsps['/chat'].in('room2').emit('message', 'i was in room two');
-    // this message will NOT go to the client defined above
-    io.sockets.in('foobar').emit('message', 'anyone in this room yet?');
-}, 5000);
+//     io.nsps['/chat'].in('room1').emit('message', 'I was in room one');
+//     io.nsps['/chat'].in('room2').emit('message', 'i was in room two');
+//     // this message will NOT go to the client defined above
+//     io.sockets.in('foobar').emit('message', 'anyone in this room yet?');
+// }, 5000);
 
+
+subClient.on("message", function(channel, message) {
+    console.log("socket with redis subscribe" + channel + ": " + message);
+    io.nsps['/chat'].in(room).emit('message', message);
+});
+
+subClient.subscribe("nonumber1989");
